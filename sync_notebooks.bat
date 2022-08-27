e38dbd959e83e0fb6f55f2dab1b521343d9488cc
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-08-27 17:50:31 -0600

    Update sync code to backup files just in case

--- sync_notebooks.bat
+++ sync_notebooks.bat
@@ -1,2 +1,17 @@
-call venv/Scripts/activate
-call jupytext --sync notebooks/*.ipynb
+@ECHO off
+
+@REM Activate the environment
+CALL venv\Scripts\activate
+
+@REM Create the notebook backup directory if it doesn't exist
+IF NOT EXIST .\notebooks\backup (
+    MKDIR .\notebooks\backup
+)
+@REM Copy all the notebooks to the backup directory
+FOR %%f IN (./notebooks/*.ipynb) DO (
+    >NUL COPY /Y /B %cd%\notebooks\%%f %cd%\notebooks\backup\%%f.bak
+)
+
+@REM Sync the notebooks
+call jupytext --sync notebooks\*.ipynb
+

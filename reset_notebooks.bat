6be480b550fe16ded47ceb8481bbd5f583572f79
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-09-07 15:22:27 -0600

    Reworked load_notebooks to reset_notebooks

---
+++ reset_notebooks.bat
@@ -0,0 +1,17 @@
+@ECHO off
+
+@REM Activate the environment
+CALL venv\Scripts\activate
+
+@REM Create the notebook backup directory if it doesn't exist
+IF NOT EXIST .\notebooks\backup (
+    MKDIR .\notebooks\backup
+)
+@REM Copy all the notebooks to the backup directory, deleting as we go.
+FOR %%f IN (./notebooks/*.ipynb) DO (
+    >NUL COPY /Y /B %cd%\notebooks\%%f %cd%\notebooks\backup\%%f.bak
+    DEL %cd%\notebooks\%%f
+)
+
+@REM Sync the notebooks
+CALL jupytext --sync notebooks/*.md

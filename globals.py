bfebe435e4b38d8f1080e420f9431f7c34ad436e
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2023-02-25 17:25:14 -0700

    Reworked file processing

---
+++ globals.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+GENERATION_ID_COLS = {"shifted", "discrete_vote", "total_agents", "distribution"}
+X_COLS = {
+    "number_of_proxies",
+    "number_of_delegators",
+}
+MECHANISM_COLS = {"coordination_mechanism", "voting_mechanism"}
+METRIC_COLS = {
+    "average_proxy_weight",
+    "error",
+    "error_as_percent_of_space",
+    "error_as_percent_of_space_abs",
+    "error_as_percent_of_space_squared",
+    "estimate",
+    "improvement",
+    "improvement_as_percent_of_space",
+    "max_proxy_weight",
+    "median_proxy_weight",
+    "min_proxy_weight",
+    "squared_error",
+}
+METRIC_COLS |= {f"shifted_diff/{metric}" for metric in METRIC_COLS}
+ADDITIONAL_METRICS = {"min", "max", "mean", "25%", "50%", "75%", "std"}
+ALL_METRIC_COLS = {
+    f"{metric}/{statistic}"
+    for metric in METRIC_COLS
+    for statistic in ADDITIONAL_METRICS
+}
+
+SORT_BY = [
+    "coordination_mechanism",
+    "voting_mechanism",
+    "distribution",
+    "shifted",
+    "discrete_vote",
+    "number_of_delegators",
+]
+
+missing_cols = set(SORT_BY) - X_COLS - GENERATION_ID_COLS - MECHANISM_COLS
+assert not missing_cols, (
+    "Sorted values should include all non-metric columns! " f"Missing {missing_cols}"
+)
+del missing_cols
+
+
+def load_data(data_dir: str) -> (pd.DataFrame, pd.DataFrame):
+    FILENAME = "16874621291821625949_shift-0.5_agents-24"
+    df_processed = pd.read_feather(f"{data_dir}/processed_{FILENAME}.arrow")
+    df_processed.sort_values(by=SORT_BY, inplace=True)
+
+    df_described = pd.read_feather(f"{data_dir}/described_{FILENAME}.arrow")
+    df_described.sort_values(by=SORT_BY, inplace=True)
+    return df_processed, df_described
--- process_file.py
+++ process_file.py
@@ -3,6 +3,8 @@ import logging
 import pandas as pd
 import winsound
 
+import globals
+
 #%% Setup
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,102 +15,137 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 DATA_DIR = "./data"
-filename = "3739165392236705654.arrow"
-METRIC_COLS = {
-    "estimate",
-    "min_proxy_weight",
-    "max_proxy_weight",
-    "average_proxy_weight",
-    "median_proxy_weight",
-}
+files = [
+    "17842264490753980280_shift-0.5_agents-16.arrow",
+    "16874621291821625949_shift-0.5_agents-24.arrow",
+    "16007220157225280629_shift-0.5_agents-512.arrow",
+]
+
 MAX_PREFERENCE = 1
 MIN_PREFERENCE = -1
 
-#%% Read
-logger.info("Reading file")
-df_raw = pd.read_feather(f"{DATA_DIR}/raw/{filename}")
-df_raw["total_agents"] = df_raw["number_of_proxies"] + df_raw["number_of_delegators"]
-df_raw["percent_delegators"] = df_raw["number_of_delegators"] / df_raw["total_agents"]
+SORT_BY = globals.SORT_BY + ["generation_id"]
 
-#%% Error
-logger.info("Merging for error. . .")
-df_merged = pd.merge(
-    df_raw,
-    df_raw[df_raw["coordination_mechanism"] == "All Agents"],
-    on=list(set(df_raw.columns) - METRIC_COLS - {"coordination_mechanism"}),
-    suffixes=("", "_all_agents"),
-)
-logger.info("Calculating error. . .")
-df_raw["error"] = df_merged["estimate"] - df_merged["estimate_all_agents"]
-METRIC_COLS.add("error")
-df_raw["error_as_percent_of_space"] = df_raw["error"] / (
-    MAX_PREFERENCE - MIN_PREFERENCE
-)
-METRIC_COLS.add("error_as_percent_of_space")
-df_raw["squared_error"] = df_raw["error"] ** 2
-METRIC_COLS.add("squared_error")
-df_raw["error_as_percent_of_space_squared"] = df_raw["error_as_percent_of_space"] ** 2
-METRIC_COLS.add("error_as_percent_of_space_squared")
-df_raw["error_as_percent_of_space_abs"] = df_raw["error_as_percent_of_space"] ** 2
-METRIC_COLS.add("error_as_percent_of_space_abs")
+for filename in files:
+    METRIC_COLS = {
+        "estimate",
+        "min_proxy_weight",
+        "max_proxy_weight",
+        "average_proxy_weight",
+        "median_proxy_weight",
+    }
 
-#%% Improvement
-logger.info("Merging for improvement. . .")
-df_merged = pd.merge(
-    df_raw,
-    df_raw[df_raw["coordination_mechanism"] == "Active Only"],
-    on=list(set(df_raw.columns) - METRIC_COLS - {"coordination_mechanism"}),
-    suffixes=("", "_active_only"),
-)
-logger.info("Calculating improvement. . .")
-df_raw["improvement"] = abs(df_merged["error_active_only"]) - abs(df_merged["error"])
-METRIC_COLS.add("improvement")
-df_raw["improvement_as_percent_of_space"] = df_raw["improvement"] / (
-    MAX_PREFERENCE - MIN_PREFERENCE
-)
-METRIC_COLS.add("improvement_as_percent_of_space")
+    #%% Read
+    logger.info(f"Reading file {filename}")
+    df_raw = pd.read_feather(f"{DATA_DIR}/raw/{filename}")
+    df_raw.sort_values(by=SORT_BY, inplace=True)
+    df_raw.index.set_names(["ID"], inplace=True)
+    df_raw.reset_index(inplace=True)
+    df_raw["total_agents"] = (
+        df_raw["number_of_proxies"] + df_raw["number_of_delegators"]
+    )
 
-#%% Shifted
-logger.info("Comparing against shifted. . .")
-df_merge = pd.merge(
-    df_raw,
-    df_raw.query("shifted == True"),
-    on=list(set(df_raw.columns) - METRIC_COLS - {"shifted"}),
-    suffixes=("", "/shifted"),
-)
-new_cols = []
-for col in METRIC_COLS:
-    df_raw[f"shifted_diff/{col}"] = df_merge[f"{col}"] - df_merge[f"{col}/shifted"]
-    new_cols.append(f"shifted_diff/{col}")
-METRIC_COLS.update(new_cols)
+    #%% Error
+    logger.info("Merging for error. . .")
+    df_merged = pd.merge(
+        df_raw,
+        df_raw.query("coordination_mechanism == 'All Agents'"),
+        on=list(set(df_raw.columns) - METRIC_COLS - {"coordination_mechanism", "ID"}),
+        suffixes=("", "_all_agents"),
+    )
 
-#%% Save processed
-logger.info("Saving processed. . .")
-df_raw.to_feather(f"{DATA_DIR}/processed_{filename}")
+    logger.info("Calculating error. . .")
+    new_cols = set()
+    df_merged["error"] = df_merged["estimate"] - df_merged["estimate_all_agents"]
+    new_cols.add("error")
+    df_merged["error_as_percent_of_space"] = df_merged["error"] / (
+        MAX_PREFERENCE - MIN_PREFERENCE
+    )
+    new_cols.add("error_as_percent_of_space")
+    df_merged["squared_error"] = df_merged["error"] ** 2
+    new_cols.add("squared_error")
+    df_merged["error_as_percent_of_space_squared"] = (
+        df_merged["error_as_percent_of_space"] ** 2
+    )
+    new_cols.add("error_as_percent_of_space_squared")
+    df_merged["error_as_percent_of_space_abs"] = (
+        df_merged["error_as_percent_of_space"] ** 2
+    )
+    new_cols.add("error_as_percent_of_space_abs")
 
-#%% Describe
-logger.info(f"Describing {len(df_raw)} rows. . .")
-df_described: pd.DataFrame = df_raw.groupby(
-    by=list(set(df_raw.columns) - METRIC_COLS - {"generation_id"})
-).describe()
+    df_raw = pd.merge(df_raw, df_merged[list(new_cols) + ["ID"]], on=["ID"])
+    METRIC_COLS |= new_cols
 
-#%% Clean up described
-logger.info("Cleaning up. . .")
-df_described.reset_index(inplace=True)
-df_described.columns = df_described.columns.map("/".join)
-df_described.rename(
-    columns={c: c[:-1] if c.endswith("/") else c for c in df_described.columns},
-    inplace=True,
-)
-df_described.drop(
-    columns=[c for c in df_described.columns if "generation_id" in c or "/count" in c],
-    inplace=True,
-)
+    #%% Improvement
+    logger.info("Merging for improvement. . .")
+    df_merged = pd.merge(
+        df_raw,
+        df_raw.query("coordination_mechanism == 'Active Only'"),
+        on=list(set(df_raw.columns) - METRIC_COLS - {"coordination_mechanism", "ID"}),
+        suffixes=("", "_active_only"),
+    )
 
-#%% Save described
-logger.info("Saving described. . .")
-df_described.to_feather(f"{DATA_DIR}/described_{filename}")
+    logger.info("Calculating improvement. . .")
+    new_cols = set()
+    df_merged["improvement"] = abs(df_merged["error_active_only"]) - abs(
+        df_merged["error"]
+    )
+    new_cols.add("improvement")
+    df_merged["improvement_as_percent_of_space"] = df_merged["improvement"] / (
+        MAX_PREFERENCE - MIN_PREFERENCE
+    )
+    new_cols.add("improvement_as_percent_of_space")
+
+    df_raw = pd.merge(df_raw, df_merged[list(new_cols) + ["ID"]], on=["ID"])
+    METRIC_COLS |= new_cols
+
+    #%% Shifted
+    logger.info("Comparing against shifted. . .")
+    df_merged = pd.merge(
+        df_raw,
+        df_raw.query("shifted == True"),
+        on=list(set(df_raw.columns) - METRIC_COLS - {"shifted", "ID"}),
+        suffixes=("", "/shifted"),
+    )
+
+    new_cols = set()
+    for col in METRIC_COLS:
+        df_merged[f"shifted_diff/{col}"] = (
+            df_merged[f"{col}"] - df_merged[f"{col}/shifted"]
+        )
+        new_cols.add(f"shifted_diff/{col}")
+
+    df_raw = pd.merge(df_raw, df_merged[list(new_cols) + ["ID"]], on=["ID"])
+    METRIC_COLS |= new_cols
+
+    #%% Save processed
+    logger.info("Saving processed. . .")
+    df_raw.to_feather(f"{DATA_DIR}/processed_{filename}")
+
+    #%% Describe
+    logger.info(f"Describing {len(df_raw)} rows. . .")
+    df_raw.drop(columns=["ID", "generation_id"], inplace=True)
+    df_described: pd.DataFrame = df_raw.groupby(
+        by=list(set(df_raw.columns) - METRIC_COLS)
+    ).describe()
+
+    #%% Clean up described
+    logger.info("Cleaning up. . .")
+    df_described.reset_index(inplace=True)
+    df_described.columns = df_described.columns.map("/".join)
+    df_described.rename(
+        columns={c: c[:-1] if c.endswith("/") else c for c in df_described.columns},
+        inplace=True,
+    )
+    df_described.drop(
+        columns=[c for c in df_described.columns if "/count" in c],
+        inplace=True,
+    )
+
+    #%% Save described
+    logger.info("Saving described. . .")
+    df_described.to_feather(f"{DATA_DIR}/described_{filename}")
 
 #%% Done
 logger.info("Done!")
-winsound.Beep(500, 100)
+winsound.Beep(750, 750)
